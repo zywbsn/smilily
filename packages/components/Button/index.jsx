4364b0f2b334773ebd5bae828b7dd9755a5999bd
silencelamb <ooooooooooos@163.com>
2023-10-03 18:10:40 +0800

    Button disabled

--- packages/components/Button/index.jsx
+++ packages/components/Button/index.jsx
@@ -1,23 +1,32 @@
 import React from "react";
+import Icon from "../Icon";
 
-const BaseClass = "py-1.5 px-3 rounded-full text-sm font-medium min-w-[90px] text-[#ffffff]";
-const PrimaryClass = BaseClass + " bg-primary-dark hover:bg-primary-dark/50 "
+const BaseClass = "px-3 py-1.5 rounded-full text-sm font-medium min-w-[90px] text-[#ffffff] ";
+const PrimaryClass = BaseClass + " bg-primary hover:bg-primary/50 "
 const ErrorClass = BaseClass + " bg-error hover:bg-error/50 "
-const TextClass = BaseClass + " text-primary-dark hover:bg-primary-dark/50 "
-const TextErrorClass = BaseClass + " text-error hover:bg-primary-dark/50 "
+const TextClass = BaseClass + " text-primary hover:bg-primary/50 "
+const TextErrorClass = BaseClass + " text-error hover:bg-primary/50 "
 const InfoClass = BaseClass + " bg-info-dark hover:bg-primary/50 "
 const SuccessClass = BaseClass + " bg-success-dark hover:bg-primary/50 "
 
-const getClassName = (type) => {
+const getClassName = (type, disabled) => {
   switch (type) {
     case "primary":
       return PrimaryClass;
     case "error":
       return ErrorClass;
     case "text":
-      return TextClass;
+      if (!disabled) {
+        return TextClass;
+      } else {
+        return " ";
+      }
     case "text error":
-      return TextErrorClass;
+      if (!disabled) {
+        return TextErrorClass;
+      } else {
+        return " ";
+      }
     case "info":
       return InfoClass;
     case "success":
@@ -25,19 +34,29 @@ const getClassName = (type) => {
   }
 };
 
+const getDisableClass = (type, disabled) => {
+  if (!["text", "text error"].includes(type) && disabled) {
+    return " bg-primary/50 text-[#C3C3C3] cursor-not-allowed ";
+  } else if (disabled) {
+    return " text-[#C3C3C3] cursor-not-allowed ";
+  }
+
+};
+
 export default React.forwardRef((props, ref) => {
-  console.log("props", props);
-  console.log("ref", ref);
-  const { children, type, onClick, className } = props;
-  console.log(PrimaryClass + className);
+  const { children, type, onClick, className, icon, size, disabled, ...rest } = props;
+  const Btn = rest.href ? "a" : "button";
+  const btnClass = getClassName(type, disabled) + getDisableClass(type, disabled) + className;
   return (
     <>
-      <button
-        onClick={onClick}
-        className={getClassName(type) + className}
+      <Btn
+        {...rest}
+        onClick={disabled ? null : onClick}
+        className={btnClass}
       >
+        {icon ? <Icon className="h-3 mr-1" name={icon} size={size || 14} /> : null}
         {children}
-      </button >
+      </Btn>
     </>
   )
 });
