1fa520e5dc8982387a728dae081e23809189c661
silencelamb <ooooooooooos@163.com>
2023-10-03 18:10:52 +0800

    Icon

--- packages/components/index.js
+++ packages/components/index.js
@@ -1,11 +1,12 @@
 import Button from "./Button/index";
 import Select from "./Select/index";
 import Radio from "./Radio/index";
+import Icon from "./Icon/index";
 
-export { Button, Select, Radio };
+export { Button, Select, Radio, Icon };
 
 const install = (App, options) => {
-  const components = [Button, Select, Radio];
+  const components = [Button, Select, Radio, Icon];
   components.forEach((component) => {
     App.component(component.name, component);
   });
