7a382650932bfbc0a75e96603bcb812fcd702b2b
silencelamb <ooooooooooos@163.com>
2023-10-03 18:11:02 +0800

    测试组件

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,47 @@
 import React, { useRef } from "react";
-import { Button, Select, Radio } from "smilily";
+import { Button, Select, Radio, Icon } from "smilily";
 
 
 
 export default () => {
+  const onButton = () => {
+    console.log("onButton");
+  };
   return (
     <>
+
       <div className="w-full min-h-screen bg-green-100 p-5">
-        <div className="mb-3">
-          <Button type="primary">中文测试</Button>
+        <div>
+          <Icon name="Add" fill="#ff0000" />
+          <Icon name="Bookmark" fill="#ff0000" />
         </div>
         <div className="mb-3">
+          <Button onClick={onButton} className="text-green-300" icon="Add" type="primary" disabled>中文测试</Button>
           <Button type="primary">smilily</Button>
         </div>
         <div className="mb-3">
           <Button type="primary">smilily Button</Button>
+          <Button type="error">smilily error</Button>
+
+        </div>
+
+        <div className="mb-3">
+          <Button type="primary" href="https://www.baidu.com">smilily a</Button>
+          <Button type="primary" disabled href="https://www.baidu.com">smilily a</Button>
+        </div>
+        <div className="mb-3">
+          <Button type="text error" title="smilily title">smilily text error</Button>
+          <Button type="text error" disabled title="smilily title">smilily text error</Button>
+        </div>
+        <div className="mb-3">
+          <Button type="text" title="smilily title">smilily text</Button>
+          <Button type="text" disabled title="smilily title">smilily text</Button>
         </div>
         <Select />
         <Radio />
-      </div>
+        <button disabled>1</button>
+
+      </div >
     </>
   );
 }
