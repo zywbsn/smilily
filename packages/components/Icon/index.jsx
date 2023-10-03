dc120575ced3a5ff42f00acf04f94ca7406951f7
silencelamb <ooooooooooos@163.com>
2023-10-03 21:31:33 +0800

    Icon

--- packages/components/Icon/index.jsx
+++ packages/components/Icon/index.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import * as Icon from "@icon-park/react";
 export default ({ name, size, ...rest }) => {
-  // console.log(name);
-  // const key = name.slice(0, 1).toUpperCase() + name.slice(1).toLowerCase();
   const Element = Icon[name];
   return (
     <>
