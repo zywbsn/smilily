962b315d6bd4410decff9b4e9015968722207d1e
silencelamb <ooooooooooos@163.com>
2023-10-04 10:21:16 +0800

    Button

--- packages/components/Button/index.jsx
+++ packages/components/Button/index.jsx
@@ -2,41 +2,50 @@ import React from "react";
 import Icon from "../Icon";
 import { tv } from "tailwind-variants";
 
-const BaseClass = "px-3 py-1.5 rounded-full text-sm font-medium min-w-[90px] text-[#ffffff] ";
-const PrimaryClass = BaseClass + " bg-primary hover:bg-primary/50 ";
-const ErrorClass = BaseClass + " bg-error hover:bg-error/50 ";
-const TextClass = BaseClass + " text-primary hover:bg-primary/50 ";
-const TextErrorClass = BaseClass + " text-error hover:bg-primary/50 ";
+// const BaseClass = "px-3 py-1.5 rounded-full text-sm font-medium min-w-[90px] text-[#ffffff] ";
+// const PrimaryClass = BaseClass + " bg-primary hover:bg-primary/50 ";
+// const ErrorClass = BaseClass + " bg-error hover:bg-error/50 ";
+// const TextClass = BaseClass + " text-primary hover:bg-primary/50 ";
+// const TextErrorClass = BaseClass + " text-error hover:bg-primary/50 ";
 
-const TypeClass = {
-  primary: PrimaryClass,
-  error: ErrorClass,
-  text: TextClass,
-  "text error": TextErrorClass
-};
+// const TypeClass = {
+//   primary: PrimaryClass,
+//   error: ErrorClass,
+//   text: TextClass,
+//   "text error": TextErrorClass
+// };
 
-const DisabledClass = {
-  primary: " text-[#C3C3C3] cursor-not-allowed ",
-  error: " text-[#C3C3C3] cursor-not-allowed ",
-  text: " text-[#C3C3C3] cursor-not-allowed ",
-  "text error": " text-[#C3C3C3] cursor-not-allowed "
+// const DisabledClass = {
+//   primary: " text-[#C3C3C3] cursor-not-allowed ",
+//   error: " text-[#C3C3C3] cursor-not-allowed ",
+//   text: " text-[#C3C3C3] cursor-not-allowed ",
+//   "text error": " text-[#C3C3C3] cursor-not-allowed "
 
-};
+// };
 
 const getClass = tv({
   base: "px-3 py-1.5 rounded-full text-sm font-medium min-w-[90px] text-[#ffffff]",
   variants: {
     type: {
       primary: "bg-primary hover:bg-primary/50",
+      default: "text-[#333333] bg-[#ffffff] hover:bg-[#ffffff]/50",
       error: "bg-error hover:bg-error/50",
       text: "text-primary hover:text-primary/70",
       "text error": "text-error hover:text-error/70"
     },
     disabled: {
-      true: "text-[#C3C3C3] cursor-not-allowed bg-primary/50 hover:bg-primary/50 hover:text-[#C3C3C3]"
+      true: "text-[#C3C3C3] cursor-not-allowed bg-[#ffffff]/50 hover:bg-[#ffffff]/50 hover:text-[#C3C3C3]"
     }
   },
   compoundVariants: [
+    {
+      type: "error",
+      className: {
+        disabled: {
+          true: "bg-error/50 hover:bg-error/50"
+        }
+      }
+    },
     {
       type: "text",
       className: "bg-transparent hover:bg-transparent"
@@ -55,7 +64,7 @@ export default React.forwardRef((props, ref) => {
   const { children, type, onClick, className, icon, size, disabled, ...rest } = props;
   const Btn = rest.href ? "a" : "button";
   // const btnClass = (disabled ? DisabledClass[type] : TypeClass[type]) + className;
-  const btnClass = "px-3 py-1.5 rounded-full text-sm font-medium min-w-[90px] text-[#ffffff] bg-primary";
+  // const btnClass = "px-3 py-1.5 rounded-full text-sm font-medium min-w-[90px] text-[#ffffff] bg-primary";
   return (
     <>
       <Btn
@@ -64,8 +73,8 @@ export default React.forwardRef((props, ref) => {
         className={
           getClass({
             type,
-            disabled,
-            className
+            className,
+            disabled
           })}
       >
         {icon ? <Icon className="h-3 mr-1" name={icon} size={size || 14} /> : null}
